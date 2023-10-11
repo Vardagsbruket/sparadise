1b818e1d79f8a354284e97148f0fe955a88a87be
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-10-11 22:08:33 +0200

    Diana dev add background image to header + fix styles fonts + style links (#18)
    * add navigation
    
    * add link to stylesheet
    
    * add grid and styling to navigation
    
    * add container mainInfo + content
    
    * add footer
    
    * add border to header and footer
    
    * add styling header
    
    * fix styles after merge
    
    * add structure to CSS-file
    
    * add header & footer to pages + pages folder
    
    * add header & footer again + remove pagesfolder
    
    * fix style on buttons in header
    
    * change style to button in header
    
    * fix problems from merge in stylesheet
    
    * add gallery + fix links to img
    
    * add background img header + font stylings
    
    * delete extra folder gallery

--- footer.js
+++ footer.js
@@ -2,14 +2,14 @@ const footer = document.createElement('template');
 footer.innerHTML = `
     <footer>
         <div class="container-footer">
-            <h3>Contact SPARADISE</h3>
+            <h3>Contact Sparadise</h3>
                 <div class="adress">
                     <p>Naverlursgatan 10</p>
                     <p>Gothenburg 42144</p>
                 </div>
                 <div class="phone-email">
                     <p>Phone: (+46)707353566</p>
-                    <p>Email: info@SPARADISE.com</p>
+                    <p>Email: info@sparadise.com</p>
                 </div>
         </div>
     </footer>
